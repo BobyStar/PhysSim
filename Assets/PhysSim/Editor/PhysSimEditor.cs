5d1fd2cbbf1612a5755e480d26f8a0a41d560774
⭐BobyStar⭐ <bobystarosiris@gmail.com>
2024-04-25 15:32:48 -0500

    Testing Context Menus and Selection

--- Assets/PhysSim/Editor/PhysSimEditor.cs
+++ Assets/PhysSim/Editor/PhysSimEditor.cs
@@ -1,8 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 using UnityEditor;
 
-public class PhysSimEditor : Editor
+public static class PhysSimEditor
 {
-    
+    [MenuItem("GameObject/PhysSim/Quick Simulation", false, 0)]
+    public static void StartQuickSim()
+    {
+        Debug.Log($"Starting Quick Sim with {Selection.count} objects.");
+    }
+
+    [MenuItem("GameObject/PhysSim/Quick Simulation", true)]
+    public static bool Validate_StartQuickSim()
+    {
+        return Selection.count > 0;
+    }
 }
