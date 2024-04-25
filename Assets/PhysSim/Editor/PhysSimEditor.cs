e153ba0ca53b257cb6cb107b288d4b07967af044
⭐BobyStar⭐ <bobystarosiris@gmail.com>
2024-04-25 15:41:32 -0500

    Added to namespace

--- Assets/PhysSim/Editor/PhysSimEditor.cs
+++ Assets/PhysSim/Editor/PhysSimEditor.cs
@@ -5,179 +5,181 @@ using UnityEngine;
 using UnityEditor;
 using Unity.EditorCoroutines.Editor;
 
-public static class PhysSimEditor
+namespace BobyStar.PhysSim
 {
-    public class PhysTransformData
+    public static class PhysSimEditor
     {
-        public Vector3 position;
-        public Quaternion rotation;
-
-        public PhysTransformData(Vector3 position, Quaternion rotation)
+        public class PhysTransformData
         {
-            this.position = position;
-            this.rotation = rotation;
-        }
-    }
+            public Vector3 position;
+            public Quaternion rotation;
 
-    public static bool isRunning;
-    public static bool isQuickSim;
-
-    public static GameObject[] simObjects;
-
-
-    private static PhysTransformData[] startData;
-    private static Rigidbody[] sceneRbs;
-    private static List<MeshCollider> addedMCols;
-    private static List<Rigidbody> addedRbs;
-    private static bool[] wereKinematics;
-
-    [MenuItem("GameObject/PhysSim/Quick Simulation", false, 0)]
-    public static void StartQuickSim()
-    {
-        if (isRunning) return;
-
-        Debug.Log("Starting Quick Sim");
-
-        isRunning = true;
-        isQuickSim = true;
-        simObjects = Selection.gameObjects;
-
-        SetupSelectedSimulation();
-
-        EditorCoroutineUtility.StartCoroutineOwnerless(PhysicsUpdate());
-    }
-
-    [MenuItem("GameObject/PhysSim/Quick Simulation", true)]
-    public static bool Validate_StartQuickSim()
-    {
-        return !isRunning && Selection.count > 0;
-    }
-
-    private static void StoreSelectedPhysTransformData()
-    {
-        startData = new PhysTransformData[simObjects.Length];
-
-        for (int i = 0; i < simObjects.Length; i++)
-        {
-            startData[i] = new PhysTransformData(
-                simObjects[i].transform.position,
-                simObjects[i].transform.rotation);
-        }
-    }
-
-    private static void WritePhysTransformDataToUndo()
-    {
-        PhysTransformData[] endData = new PhysTransformData[simObjects.Length];
-
-        for (int i = 0; i < simObjects.Length; i++)
-        {
-            endData[i] = new PhysTransformData(
-                simObjects[i].transform.position, 
-                simObjects[i].transform.rotation);
-
-            simObjects[i].transform.SetPositionAndRotation(startData[i].position, startData[i].rotation);
-        }
-
-
-        for (int i = 0; i < simObjects.Length; i++)
-        {
-            Undo.RecordObject(simObjects[i].transform, $"PhysSim {(isQuickSim ? "Quick" : "Regular")} Simulation");
-
-            simObjects[i].transform.SetPositionAndRotation(endData[i].position, endData[i].rotation);
-        }
-    }
-
-    private static void SetupSelectedSimulation()
-    {
-        StoreSelectedPhysTransformData();
-
-        sceneRbs = Object.FindObjectsOfType<Rigidbody>();
-
-        wereKinematics = new bool[sceneRbs.Length];
-
-        int i = -1;
-        foreach (Rigidbody rb in sceneRbs)
-        {
-            i++;
-            wereKinematics[i] = rb.isKinematic;
-            
-            if (simObjects.Contains(rb.gameObject)) continue;
-
-            rb.isKinematic = true;
-        }
-
-        addedMCols = new List<MeshCollider>();
-        addedRbs = new List<Rigidbody>();
-        foreach (GameObject gO in simObjects)
-        {
-            if (!gO.TryGetComponent(out Collider col))
+            public PhysTransformData(Vector3 position, Quaternion rotation)
             {
-                if (!gO.TryGetComponent(out MeshFilter mFilter))
-                    continue;
-
-                addedMCols.Add(gO.AddComponent<MeshCollider>());
-                addedMCols[^1].sharedMesh = mFilter.sharedMesh;
-                addedMCols[^1].convex = true;
-            }
-
-            if (gO.TryGetComponent(out Rigidbody rb))
-            {
-                addedRbs.Add(rb);
-                rb.isKinematic = false;
-            }
-            else
-            {
-                addedRbs.Add(gO.AddComponent<Rigidbody>());
+                this.position = position;
+                this.rotation = rotation;
             }
         }
-    }
 
-    private static void EndSelectedSimulation()
-    {
-        for (int i = 0; i < sceneRbs.Length; i++)
+        public static bool isRunning;
+        public static bool isQuickSim;
+
+        public static GameObject[] simObjects;
+
+        private static PhysTransformData[] startData;
+        private static Rigidbody[] sceneRbs;
+        private static List<MeshCollider> addedMCols;
+        private static List<Rigidbody> addedRbs;
+        private static bool[] wereKinematics;
+
+        [MenuItem("GameObject/PhysSim/Quick Simulation", false, 0)]
+        public static void StartQuickSim()
         {
-            sceneRbs[i].isKinematic = wereKinematics[i];
+            if (isRunning) return;
+
+            Debug.Log("Starting Quick Sim");
+
+            isRunning = true;
+            isQuickSim = true;
+            simObjects = Selection.gameObjects;
+
+            SetupSelectedSimulation();
+
+            EditorCoroutineUtility.StartCoroutineOwnerless(PhysicsUpdate());
         }
 
-        foreach (MeshCollider mCol in addedMCols)
-            Object.DestroyImmediate(mCol);
-
-        foreach (Rigidbody rb in addedRbs)
-            Object.DestroyImmediate(rb);
-
-        WritePhysTransformDataToUndo();
-    }
-
-    public static void EndSimulation()
-    {
-        Debug.Log("Ending Simulation");
-
-        isRunning = false;
-        EndSelectedSimulation();
-    }
-
-    public static IEnumerator PhysicsUpdate()
-    {
-        Debug.Log("Starting Simulation.");
-        bool autoSim = Physics.autoSimulation;
-        Physics.autoSimulation = false;
-
-        yield return null;
-
-        float timer = 10;
-        while (timer > 0 && isRunning)
+        [MenuItem("GameObject/PhysSim/Quick Simulation", true)]
+        public static bool Validate_StartQuickSim()
         {
-            timer -= isQuickSim ? Time.fixedDeltaTime : Time.unscaledDeltaTime;
-
-            Physics.Simulate(Time.fixedDeltaTime);
-
-            Debug.Log($"Time left: {timer}.");
-            yield return isQuickSim ? null : new WaitForSecondsRealtime(Time.fixedDeltaTime);
+            return !isRunning && Selection.count > 0;
         }
 
-        Physics.autoSimulation = autoSim;
+        private static void StoreSelectedPhysTransformData()
+        {
+            startData = new PhysTransformData[simObjects.Length];
 
-        if (isRunning)
-            EndSimulation();
+            for (int i = 0; i < simObjects.Length; i++)
+            {
+                startData[i] = new PhysTransformData(
+                    simObjects[i].transform.position,
+                    simObjects[i].transform.rotation);
+            }
+        }
+
+        private static void WritePhysTransformDataToUndo()
+        {
+            PhysTransformData[] endData = new PhysTransformData[simObjects.Length];
+
+            for (int i = 0; i < simObjects.Length; i++)
+            {
+                endData[i] = new PhysTransformData(
+                    simObjects[i].transform.position,
+                    simObjects[i].transform.rotation);
+
+                simObjects[i].transform.SetPositionAndRotation(startData[i].position, startData[i].rotation);
+            }
+
+
+            for (int i = 0; i < simObjects.Length; i++)
+            {
+                Undo.RecordObject(simObjects[i].transform, $"PhysSim {(isQuickSim ? "Quick" : "Regular")} Simulation");
+
+                simObjects[i].transform.SetPositionAndRotation(endData[i].position, endData[i].rotation);
+            }
+        }
+
+        private static void SetupSelectedSimulation()
+        {
+            StoreSelectedPhysTransformData();
+
+            sceneRbs = Object.FindObjectsOfType<Rigidbody>();
+
+            wereKinematics = new bool[sceneRbs.Length];
+
+            int i = -1;
+            foreach (Rigidbody rb in sceneRbs)
+            {
+                i++;
+                wereKinematics[i] = rb.isKinematic;
+
+                if (simObjects.Contains(rb.gameObject)) continue;
+
+                rb.isKinematic = true;
+            }
+
+            addedMCols = new List<MeshCollider>();
+            addedRbs = new List<Rigidbody>();
+            foreach (GameObject gO in simObjects)
+            {
+                if (!gO.TryGetComponent(out Collider col))
+                {
+                    if (!gO.TryGetComponent(out MeshFilter mFilter))
+                        continue;
+
+                    addedMCols.Add(gO.AddComponent<MeshCollider>());
+                    addedMCols[^1].sharedMesh = mFilter.sharedMesh;
+                    addedMCols[^1].convex = true;
+                }
+
+                if (gO.TryGetComponent(out Rigidbody rb))
+                {
+                    addedRbs.Add(rb);
+                    rb.isKinematic = false;
+                }
+                else
+                {
+                    addedRbs.Add(gO.AddComponent<Rigidbody>());
+                }
+            }
+        }
+
+        private static void EndSelectedSimulation()
+        {
+            for (int i = 0; i < sceneRbs.Length; i++)
+            {
+                sceneRbs[i].isKinematic = wereKinematics[i];
+            }
+
+            foreach (MeshCollider mCol in addedMCols)
+                Object.DestroyImmediate(mCol);
+
+            foreach (Rigidbody rb in addedRbs)
+                Object.DestroyImmediate(rb);
+
+            WritePhysTransformDataToUndo();
+        }
+
+        public static void EndSimulation()
+        {
+            Debug.Log("Ending Simulation");
+
+            isRunning = false;
+            EndSelectedSimulation();
+        }
+
+        public static IEnumerator PhysicsUpdate()
+        {
+            Debug.Log("Starting Simulation.");
+            bool autoSim = Physics.autoSimulation;
+            Physics.autoSimulation = false;
+
+            yield return null;
+
+            float timer = 10;
+            while (timer > 0 && isRunning)
+            {
+                timer -= isQuickSim ? Time.fixedDeltaTime : Time.unscaledDeltaTime;
+
+                Physics.Simulate(Time.fixedDeltaTime);
+
+                Debug.Log($"Time left: {timer}.");
+                yield return isQuickSim ? null : new WaitForSecondsRealtime(Time.fixedDeltaTime);
+            }
+
+            Physics.autoSimulation = autoSim;
+
+            if (isRunning)
+                EndSimulation();
+        }
     }
 }
